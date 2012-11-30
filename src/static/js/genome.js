5040f032b6e302961e4256cbac858fc6092f6e4e
her0e1c1 <her0e1c1@gmail.com>
2012-11-30 19:45:07 +0900

    プロパティpositionをrelativeにして、プロパティleftの値を更新することで、画像の左右のスクロールを再現

--- src/static/js/genome.js
+++ src/static/js/genome.js
@@ -3,7 +3,10 @@ var HOST = "http://localhost:5000"
 
 
 function _DEBUG(){
-	var txt;
+	var txt = "";
+	left = $("#show_images").position().left;
+	txt += "; left:" + left;
+
 	$("#debug").text(txt);
 }
 
@@ -18,7 +21,7 @@ function _DEBUG(){
  *得られた画像を表示する
  */
 	var genome = function(){
-		this.init()
+		this.init.apply(this, arguments)
 	};
 
 	//public methods
@@ -29,6 +32,17 @@ function _DEBUG(){
 			this.view = new Array([0,0]);  // 表示する位置 (x, y)で表示
 			this.point = new Array([0,0]);  // 画像データの位置 (x, y)で表示
 			this.URL = window.document.URL;
+
+			//css("left")がブラウザによって異なる
+			$("#show_images").css("left", 0)
+
+			//event 登録
+			$("#controller_button_left").click(function(){
+				_update(-1)
+			});
+			$("#controller_button_right").click(function(){
+				_update(+1)
+			});
 
 		},
 		test: function(){
@@ -50,6 +64,25 @@ function _DEBUG(){
 		return eval("(" + data + ")");
 	};
 
+	/*
+	  left_or_right
+	  -1の場合　左へ更新
+	  +1の場合　右へ更新
+	*/
+	function _update(left_or_right, mode){
+		var n, left;
+		n = $("#show_images");
+		left = _px2int(n.css("left"));
+		left += 10 * left_or_right;
+		n.css("left", left)
+	}
+
+	//cssの値でpxの場合、とって数値にする
+	//100px(文字列) => 100(数値)
+	function _px2int(str){
+		return parseInt(str.replace("px" ,""))
+	}
+
 	//グローバル空間に登録する
 	window.genome = genome;
 
@@ -60,10 +93,13 @@ function _DEBUG(){
 window.onload = function(){
 	setInterval(_DEBUG, 1)
 	//モジュールを呼び出す
+	$("#show_images").append("<img />");
+	$("img").attr("src","/static/images/sample.png")
+
 	var g = new genome();
 
 	//debug code
-	$("#show_images").append("<img />");
-	$("img").attr("src","/static/images/sample.png")
-	
+//$("#controller_button_left").click();
 }
+
+//function update(){}
