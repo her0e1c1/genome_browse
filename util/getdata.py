7fef2b4112f871b33f692199da56881d71365605
her0e1c1 <her0e1c1@gmail.com>
2012-12-10 19:18:46 +0900

    画像をサーバーから取得するモジュールの作成中です。

--- util/getdata.py
+++ util/getdata.py
@@ -1,28 +1,17 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import requests
 import os
 import sys
 import time
 import getpass
+import argparse
+import requests
 from BeautifulSoup import BeautifulSoup
 
 
-def sample_main():
-    gbd = GetGraphicData()
-    if len(sys.argv) == 5:
-        try:
-            gbd._start = int(sys.argv[2])
-            gbd._end = int(sys.argv[3])
-            gbd._width = int(sys.argv[4])
-        except:
-            print("the argv of sample(start end span) are int")
-            sys.exit()
-    gbd.get_sample_data()
-
 #分割した画像データ
 class GraphicData:
-    
+
     def __init__(self,track, path, start, end):
         self.track = track
         self.path = path
@@ -46,8 +35,8 @@ class GetGraphicData:
         self._auth = requests.auth.HTTPDigestAuth(self._user, self._passwd)
         self._path = {"thaliana":"/gb2/gbrowse/thaliana/"}
         #一枚の画像サイズ
-        self._width = 1000 
-        
+        self._width = 1000
+
         #gbrowseにポストするデータ群
         self._add_tracks = [
             "Ahal_200_L1_CoverageXyplot",
@@ -56,7 +45,7 @@ class GetGraphicData:
         self._name = [
             "Chr1"
             ]
-        
+
 
         #ゲノム配列を取得する長さ 300K
         self._span = 300 * (10 ** 3)
@@ -72,42 +61,17 @@ class GetGraphicData:
         self._directory_to_save = dir
         if not os.path.isdir(dir):
             os.mkdir(dir)
-        
+
         for track in self._add_tracks:
             dir = self._directory_to_save + track + "/"
             if not os.path.isdir(dir):
                 os.mkdir(dir)
 
-    #設定ファイルをまとめて書き換えるようにする
-    def set(self):
-        pass
-
-    #プライベート変数にアクセスする関数
-    def get_add_tracks(self):
-        return self._add_tracks
-
-    def get_name(self):
-        """
-        どのデータを読み出すのか決める
-        汎用性を持たせる予定
-        """
-        return self._name[0]
-
-    def get_uri(self):
-        """uriの参照は、ここから行うこと"""
-        uri = self._protocol + self._host
-        uri += self._path["thaliana"]
-        return uri
-
     #requests.get/postのラッパー
     def get(self, uri=None):
-        if uri is None:
-            uri = self.get_uri()
         return requests.get(uri, auth=self._auth, cookies=self._cookies)
 
     def post(self, uri=None, p=None):
-        if uri is None:
-            uri = self.get_uri()
         return requests.post(uri,p, auth=self._auth, cookies=self._cookies)
 
 
@@ -123,7 +87,7 @@ class GetGraphicData:
         for gd in gd_list:
             self._save_image(gd)
 
-       
+
     def _save_image(self, gd):
         if not isinstance(gd,GraphicData):
             print("error: can't save this GraphicData", gd)
@@ -140,7 +104,7 @@ class GetGraphicData:
         f.close()
         print("save:{0}".format(name))
 
-    
+
     def _first_login(self):
         """
         ログイン出来るか調べる
@@ -157,7 +121,7 @@ class GetGraphicData:
             print("error : no session cookies")
             sys.exit()
         self._cookies = res.cookies
-        
+
         #初めに初期設定を行う
         self._config_after_login()
 
@@ -184,7 +148,7 @@ class GetGraphicData:
             }
         uri = self.get_uri()
         res = self.post(p=post)
-        
+
         paths = self._post_data_to_get_imagepath()
         gd_list = []
         for (track, path) in paths.items():
@@ -242,7 +206,7 @@ class GetGraphicData:
         print("get uri:", paths)
 
         return paths
-        
+
 
     def _config_after_login(self):
         """
@@ -259,23 +223,23 @@ class GetGraphicData:
             "width": self._width,
             }
         self.post(p=post)
-        
+
         for track in self.get_add_tracks():
             post = {
                 "action" : "add_tracks",
                 "track_names" : track
                 }
-                
+
             res = self.post(p=post)
             key = res.json["track_data"][track]["track_key"]
-            
+
             post = {
                 "action" : "retrieve_multiple",
                 "track_ids": track,
                 "tk_" + track: key
                 }
             res = self.post(p= post)
-            
+
             post = {
                 "action" : "set_track_visibility",
                 "visible" : 1,
@@ -285,21 +249,218 @@ class GetGraphicData:
 
         return True
 
+
+class GetGBrowseData:
+
+    def __init__(self, **conf):
+        """
+        辞書データを受け取って初期化します。
+        """
+
+        self._url = conf["url"]
+        self._user = conf["user"]
+        self._passwd =conf["passwd"]
+        self._auth = requests.auth.HTTPDigestAuth(self._user, self._passwd)
+        self._seq_id = conf["seq_id"]
+        self._cookies = None
+
+    #requests.get/postのラッパー
+    def get(self, url=None):
+        if url is None:
+            url = self._url()
+        return requests.get(url, auth=self._auth, cookies=self._cookies)
+
+
+    def post(self, url=None, p=None):
+        if url is None:
+            url = self._url()
+        return requests.post(url,p, auth=self._auth, cookies=self._cookies)
+
+
+    #serverへpostする関数
+    def _post_from_to_genome(self, start, end):
+        """
+        どこからどこまでの塩基配列を受け取るのかを決めます。
+
+        以下のような文字列を生成させてserverにpostします。
+        ch1:10,000..20,000
+        """
+        name = "{id}:{start}..{end}".format(
+            id=self._seq_id,
+            start=start,
+            end=end)
+
+        #firbugでpostされていたデータを含んでいる
+        post = {
+            "name" : name,
+            "force_submit" : 0,
+            "plugin_find" : 0
+            }
+        res = self.post(p=post)
+
+        return res
+        # paths = self._post_data_to_get_imagepath()
+        # gd_list = []
+        # for (track, path) in paths.items():
+        #    gd_list.append(GraphicData(track, path, start, end))
+
+        # return gd_list
+
+
+    def _post_data_to_get_imagepath(self):
+        """
+        画像のパスを受け取る為に、データをポストする
+        このポストする値は、firebugでネットワークを監視した値と同じもの
+        画像のパスはハッシュ化されているので、簡単にダウンロードできない
+        ３回ポストするが、１回目のポストでkeyを受け取り、
+        ３回目のポストで、そのkeyもポストしないといけない
+        最後の３回目でようやく画像のパスが手に入る
+
+        引数：なし
+        戻り値：うけとったパスの辞書
+        """
+        post = {
+            "action" : "navigate",
+            "navigate" : "left%200",
+            "view_start" :"NaN",
+            "view_stop":"NaN:",
+            "snapshot" : "false",
+            }
+        res = self.post(p=post)
+        track_keys = res.json["track_keys"]
+        tk_track_keys = {}
+        for x,y in track_keys.items():
+            tk_track_keys["tk_" + x] = y
+
+        post = {
+            "action" : "update_sections",
+            "section_names" : ["page_title","span","galaxy_form","search_form_objects"]
+            }
+        res = self.post(p=post)
+
+        post = {
+            "action" : "retrieve_multiple",
+            "track_ids" : ["Gene","Ahal_200_L1_CoverageXyplot","Ahal_200_L1_Reads","AthalCDS"]
+            }
+        post.update(tk_track_keys)
+        res = self.post(p=post)
+
+        paths = {}
+        html = res.json["track_html"]
+        for track in html.keys():
+            if track in self._add_tracks:
+                soup = BeautifulSoup(html[track])
+                try:
+                    paths[track] = soup.find("img",{"id":track + "_image"}).get("src")
+                except:
+                    pass
+        print("get uri:", paths)
+
+        return paths
+
+
+    def _first_login(self):
+        """
+        ログイン出来るか調べます。
+        また、一回目のアクセスでsession用のクッキーを取り出します。
+        """
+
+        res = requests.get()
+        if res.status_code != 200:
+            print("login error")
+            sys.exit()
+        elif res.cookies == "" or res.cookies == None:
+            print("error : no session cookies")
+            sys.exit()
+
+        print("login success!")
+        self._cookies = res.cookies
+        #初期設定します。
+        self._config_after_login()
+
+
+    def _config_after_login(self):
+        """
+        ここで行うことは以下の通りです。
+        (1)画像サイズの指定
+        (2)トラックの指定
+
+        トラックの指定は少し面倒ですが、
+        firebugを真似して書いています。
+        """
+
+        #画像サイズ決定
+        post = {
+            "action": "set_display_option",
+            "width": self._width,
+            }
+        self.post(p=post)
+
+        #トラックの指定
+        for track in self._add_tracks():
+            post = {
+                "action" : "add_tracks",
+                "track_names" : track
+                }
+
+            res = self.post(p=post)
+            key = res.json["track_data"][track]["track_key"]
+
+            post = {
+                "action" : "retrieve_multiple",
+                "track_ids": track,
+                "tk_" + track: key
+                }
+            res = self.post(p= post)
+
+            post = {
+                "action" : "set_track_visibility",
+                "visible" : 1,
+                "track_name" : track
+                }
+            self.post(p=post)
+
+
+
+"""
+予めデータを取得するための設定事項を書きます。
+分割するのに必要なデータとは分けて書きます。
+"""
+
+#取得したデータを保存するルートディレクトリを決める
+dir = os.path.join(os.getcwd(), "data")
+if not os.path.isdir(dir):
+    os.mkdir(dir)
+
+tracks = ["Ahal_200_L1_CoverageXyplot",
+          "Ahal_200_L1_Reads"]
+CONF = {
+    "host": "http://gbrowse.seselab.org",
+    "path": "/gb2/gbrowse/thaliana",
+    "max_length":  300 * (10 ** 6),  # 30M
+    "max_width": 10 * (10 ** 3),  # 10k
+    "passwd": getpass.getpass("Password:"),
+    "user": getpass.getpass("User:"),
+    "seq_id": "Chr1",
+    "width": 800,  # 一枚の画像サイズ
+    "save_dir": dir,
+    "tracks": tracks,
+}
+
+def main():
+
+    p = argparse.ArgumentParser()
+    p.add_argument("start")
+    p.add_argument("stop")
+    p.add_argument("width")
+    args = p.parse_args()
+    1/0
+
 if __name__ == "__main__":
     #計測時間
-    start = time.time()   
-    
-    if len(sys.argv) < 2:
-        print("Usage: 引数が最低２つ必要です")
-        print("cmd sample [start end width]")
-        sys.exit()
-    a = sys.argv[1]
-    if a == "sample":
-        sample_main()
-    if a == "test":
-        test()
-
-    end = time.time()    
-    fmt = "time {0}".format(end - start)
+    start = time.time()
+    main()
+    stop = time.time()
+    fmt = "time {0}".format(stop - start)
     print("-" * 50)
     print(fmt)
